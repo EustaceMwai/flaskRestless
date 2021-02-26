00dce2f05671f2790da7536c5dddf17f18a2685c
EustaceMwai <43333927+EustaceMwai@users.noreply.github.com>
2021-02-26 14:51:47 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restless import APIManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'api.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    items = db.relationship('Item', backref='user', lazy='dynamic')
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
