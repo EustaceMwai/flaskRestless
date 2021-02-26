369f89ea89480507d96b57df9582789081590fd2
EustaceMwai <43333927+EustaceMwai@users.noreply.github.com>
2021-02-26 22:13:14 +0300

    create_api

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'api.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ap2.db'
 db = SQLAlchemy(app)
+manager = APIManager(app, flask_sqlalchemy_db=db)
 
 
 class User(db.Model):
@@ -17,6 +18,10 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+manager.create_api(User, methods=['GET', 'POST', 'PUT'])
+manager.create_api(Item, methods=['GET', 'POST', 'DELETE', 'PATCH'], allow_delete_many=True, allow_patch_many=True)
 
 
 if __name__ == '__main__':
